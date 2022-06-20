613f1767c7d36a7cb944db00dbc5ce5f18a6767a
Tiago Collot <collot.tiago1@gmail.com>
2022-06-20 16:19:41 +0100

    created app.jsx and flats search input

---
+++ airbnb/src/components/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="app">
+      <div className="main">
+        <input className="search" />
+        <div className="flats">ist of flats here!</div>
+      </div>
+
+    </div>
+  )
+}
