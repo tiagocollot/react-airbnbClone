2c4d5d158ea4e67fc5b88a83c5fc098eb2bfd764
Tiago Collot <collot.tiago1@gmail.com>
2022-06-21 16:22:40 +0100

    Created flats.js and added an 'API' with flats and respectve prices

--- airbnb-clone/src/components/App.jsx
+++ airbnb-clone/src/components/App.jsx
@@ -1,30 +1,34 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "./App.scss";
 
 import Flat from "./Flat";
 
 const App = () => {
+  const [flats ,setFlats] = useState([]);
   return (
     <div className="app">
       <div className="main">
         <input className="search" />
         <div className="flats">
-          <Flat
-            price={100}
+          {flats.map((id, ...props) => {
+            return <Flat key={id} {...props} />
+          })}
+          {/* <Flat
+            price={380}
             name="Beautiful Flat"
             image="https://images.unsplash.com/photo-1451153378752-16ef2b36ad05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1467&q=80"
           />
           <Flat
-            price={98}
+            price={430}
             name="Terrible Flat"
             image="https://images.unsplash.com/photo-1451153378752-16ef2b36ad05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1467&q=80"
           />
           <Flat
-            price={10}
+            price={400}
             name="River view flat near the shore"
             image="https://images.unsplash.com/photo-1451153378752-16ef2b36ad05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1467&q=80"
-          />
+          /> */}
         </div>
       </div>
       <div className="map"></div>
